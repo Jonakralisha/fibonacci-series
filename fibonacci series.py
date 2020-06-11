b1dbd445a992d7bd9c3fbfefdc49b36c01e16c8a
Jonakralisha <66684253+Jonakralisha@users.noreply.github.com>
2020-06-11 20:59:50 +0530

    Add files via upload

---
+++ fibonacci series.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n=int(input("ENTER THE VALUE OF n:"))
+sum=0
+count=1
+a=0
+b=1
+while(count<=n):
+    print(sum)
+    count=count+1
+    a=b
+    b=sum
+    sum=a+b
+
+
+# In[ ]:
+
+
+
+
